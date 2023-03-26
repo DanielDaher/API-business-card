0c9c565c27a0674d169d80ce5682296a276f65ca
Daniel <danielfdoficial@gmail.com>
2023-03-26 15:06:02 -0300

    init API and install dependencies

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json({
+  type: ['application/json', 'text/plain']
+})); //https://stackoverflow.com/questions/54016068/empty-body-in-fetch-post-request
+
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
